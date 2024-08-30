dc7cec3e75b4ffd769a4d17633f6883f744d8dc9
Lutreze Hue Jacinto <86185117+nocturnalDev11@users.noreply.github.com>
2024-08-30 23:10:36 +0800

    commit

---
+++ bootstrap/app.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Foundation\Application;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function ($middleware) {
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\AdminMiddleware::class,
+            'auth' => \App\Http\Middleware\Authenticate::class,
+            'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        ]);
+    })
+    ->withExceptions(function ($exceptions) {
+        //
+    })->create();
