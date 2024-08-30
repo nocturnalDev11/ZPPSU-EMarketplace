2c699f14d8c6426be05b9e3b900fb05611605333
Lutreze Hue Jacinto <86185117+nocturnalDev11@users.noreply.github.com>
2024-08-30 12:11:04 -0400

    Update SearchContacts.php

--- app/Livewire/Messages/SearchContacts.php
+++ app/Livewire/Messages/SearchContacts.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Messages;
 
 use App\Models\User;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class SearchContacts extends Component
 {
@@ -18,12 +19,25 @@ class SearchContacts extends Component
     public function render()
     {
         $users = User::query()
-            ->where('first_name', 'like', '%' . $this->search . '%')
-            ->orWhere('last_name', 'like', '%' . $this->search . '%')
-            ->get();
+            ->where(function($query) {
+                $query->where('first_name', 'like', '%' . $this->search . '%')
+                    ->orWhere('last_name', 'like', '%' . $this->search . '%');
+            })
+            ->whereHas('messages')
+            ->with(['sentMessages' => function ($query) {
+                $query->where('recipient_id', Auth::id())->latest();
+            }, 'receivedMessages' => function ($query) {
+                $query->where('sender_id', Auth::id())->latest();
+            }])
+            ->get()
+            ->map(function ($userItem) {
+                $userItem->latestMessage = $userItem->sentMessages->merge($userItem->receivedMessages)->sortByDesc('created_at')->first();
+                return $userItem;
+            });
 
         return view('livewire.messages.search-contacts', [
             'users' => $users,
         ]);
     }
+
 }
