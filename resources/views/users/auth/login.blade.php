b73c41eb5a7fe7624f7b58c5810af04579303b8b
Lutreze Hue Jacinto <86185117+nocturnalDev11@users.noreply.github.com>
2024-09-06 08:01:37 -0400

    Update login.blade.php

--- resources/views/users/auth/login.blade.php
+++ resources/views/users/auth/login.blade.php
@@ -1,60 +1,98 @@
 @extends('layouts.nav')
 
 @section('content')
-<div class="flex h-screen bg-slate-100 dark:bg-slate-800">
-    <div class="mx-auto w-1/2 flex items-center justify-center">
-        <div class="w-full max-w-md sm:max-w-lg md:max-w-2xl lg:max-w-xl xl:max-w-md bg-white p-4 md:p-8 lg:p-10 rounded-lg shadow-md border dark:bg-slate-700 dark:border-slate-600">
-            <div class="flex flex-col">
-                <h1 class="text-3xl font-semibold text-slate-600 text-center">
-                    <span class="text-red-700 dark:text-red-600">ZPPSU</span>
-                    <span class="text-yellow-600 dark:text-yellow-500">E-Marketplace</span>
-                </h1>
-                <h1 class="text-3xl font-semibold mb-6 text-center dark:text-slate-200">
-                    {{ __('Login') }}
-                </h1>
+<div class="font-[sans-serif]">
+    <div class="min-h-screen flex fle-col items-center justify-center py-6 px-4">
+        <div class="grid md:grid-cols-2 items-center gap-10 max-w-6xl w-full">
+            <div>
+                <h2 class="lg:text-5xl text-4xl font-extrabold lg:leading-[55px] text-gray-800">
+                    Seamless Login for Exclusive Access
+                </h2>
+                <p class="text-sm mt-6 text-gray-800">Immerse yourself in a hassle-free login journey with our intuitively designed login form. Effortlessly access your account.</p>
+                <p class="text-sm mt-12 text-gray-800">Don't have an account <a href="{{ route('register') }}" wire:navigate class="text-red-700 font-semibold hover:underline ml-1">Register here</a></p>
             </div>
-            <h1 class="text-sm font-semibold mb-6 text-slate-500 text-center dark:text-slate-300">Access your university community anytime, anywhere, for free.</h1>
-            <form action="{{ route('login') }}" method="POST" class="space-y-4">
+            <form action="{{ route('login') }}" method="POST" class="max-w-md md:ml-auto w-full">
                 @csrf
-                <!-- University ID -->
-                <div class="mb-5">
-                    <div class="relative">
-                        <input type="university_id" id="university_id" name="university_id" aria-describedby="outlined_error_university_id" class="block px-2.5 pb-2.5 pt-4 w-full text-xl text-slate-900 bg-transparent rounded-lg border border-slate-300 appearance-none dark:text-white dark:border-slate-400 dark:focus:border-orange-400 focus:outline-none focus:ring-0 focus:border-orange-500 peer @error('university_id') is-invalid @enderror" placeholder=" " value="{{ old('university_id') }}" required />
-                        <label for="university_id" class="absolute text-2xl text-slate-500 dark:text-slate-400 duration-300 transform -translate-y-6 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-slate-800 px-2 peer-focus:px-2 peer-focus:text-orange-400 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-6 start-3 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
-                            {{ __('University ID') }}
-                        </label>
-                    </div>
-                    @error('university_id')
-                        <p id="outlined_error_university_id" class="mt-2 text-xs text-red-700 dark:text-red-500">
-                            <span class="font-medium">
-                                {{ $message }}
+                <h3 class="text-gray-800 text-3xl font-extrabold mb-8">
+                    Sign in
+                </h3>
+                <div class="space-y-4">
+                    <div>
+                        <label for="university_id" class="text-gray-800 text-sm mb-2 block">University ID</label>
+                        <div class="relative flex items-center">
+                            <input type="text" id="university_id" name="university_id" required class="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300 @error('univeristy_id') is-invalid @enderror" placeholder="Enter University ID" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-4 h-4 absolute right-4" viewBox="0 0 24 24">
+                                <circle cx="10" cy="7" r="6" data-original="#000000"></circle>
+                                <path d="M14 15H6a5 5 0 0 0-5 5 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 5 5 0 0 0-5-5zm8-4h-2.59l.3-.29a1 1 0 0 0-1.42-1.42l-2 2a1 1 0 0 0 0 1.42l2 2a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-.3-.29H22a1 1 0 0 0 0-2z" data-original="#000000"></path>
+                            </svg>
+                        </div>
+                        @error('university_id')
+                            <span class="text-red-500 text-sm mt-1 block" role="alert">
+                                <strong>{{ $message }}</strong>
                             </span>
-                        </p>
-                    @enderror
-                </div>
-                <!-- Password -->
-                <div class="mb-4">
-                    <div class="relative">
-                        <input type="password" id="password" name="password" aria-describedby="outlined_error_password" class="block px-2.5 pb-2.5 pt-4 w-full text-xl text-slate-900 bg-transparent rounded-lg border-slate-300 appearance-none dark:text-white dark:border-slate-600 dark:focus:border-orange-400 focus:outline-none focus:ring-0 focus:border-orange-500 peer @error('password') is-invalid @enderror" placeholder=" " value="{{ old('password') }}" required/>
-                        <label for="password" class="absolute text-2xl text-slate-500 dark:text-slate-400 duration-300 transform -translate-y-6 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-slate-800 px-2 peer-focus:px-2 peer-focus:text-orange-400 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-6 start-3 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
-                            {{ __('Password') }}
-                        </label>
+                        @enderror
                     </div>
-                    @error('password')
-                        <p id="outlined_error_password" class="mt-2 text-xs text-red-700 dark:text-red-500">
-                            <span class="font-medium">
-                                {{ $message }}
+
+                    <div x-data="{ showPassword: false }">
+                        <label for="password" class="text-gray-800 text-sm mb-2 block">Password</label>
+                        <div class="relative flex items-center">
+                            <input name="password" :type="showPassword ? 'text' : 'password'" required class="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300 @error('password') is-invalid @enderror" placeholder="Enter password" />
+                            <svg @click="showPassword = !showPassword" xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-4 h-4 absolute right-4 cursor-pointer" viewBox="0 0 128 128">
+                                <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
+                            </svg>
+                        </div>
+                        @error('password')
+                            <span class="text-red-500 text-sm mt-1 block" role="alert">
+                                <strong>{{ $message }}</strong>
                             </span>
-                        </p>
-                    @enderror
+                        @enderror
+                    </div>
+
+                    {{-- <div class="flex flex-wrap items-center justify-between gap-4">
+                        <div class="flex items-center">
+                            <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded" />
+                            <label for="remember-me" class="ml-3 block text-sm text-gray-800">
+                                Remember me
+                            </label>
+                        </div>
+                        <div class="text-sm">
+                            <a href="jajvascript:void(0);" class="text-blue-600 hover:text-blue-500 font-semibold">
+                                Forgot your password?
+                            </a>
+                        </div>
+                    </div> --}}
                 </div>
-                <!-- Login Button -->
-                <div class="flex justify-center mb-4">
-                    <button type="submit" class="w-full py-3 text-2xl bg-gradient-to-br from-red-500 to-orange-400 hover:bg-gradient-to-bl text-white font-semibold rounded-md transition-colors duration-300">{{ __('Login') }}</button>
+                <div class="!mt-8">
+                    <button type="submit" class="w-full shadow-xl py-2.5 px-4 text-sm font-semibold rounded text-white bg-gradient-to-br from-orange-400 via-red-600 to-red-800 hover:bg-gradient-to-bl focus:outline-none">
+                        Log in
+                    </button>
                 </div>
-                <div class="text-sm text-slate-600 text-center dark:text-slate-300">
-                    {{ __('No account?') }} <a href="{{ route('register') }}" wire:navigate class="text-maroon hover:underline dark:text-yellow-500">{{ __('Sign Up') }}</a>
-                </div>
+                {{-- <div class="space-x-6 flex justify-center mt-8">
+                    <button type="button"
+                        class="border-none outline-none">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32px" viewBox="0 0 512 512">
+                            <path fill="#fbbd00" d="M120 256c0-25.367 6.989-49.13 19.131-69.477v-86.308H52.823C18.568 144.703 0 198.922 0 256s18.568 111.297 52.823 155.785h86.308v-86.308C126.989 305.13 120 281.367 120 256z" data-original="#fbbd00" />
+                            <path fill="#0f9d58" d="m256 392-60 60 60 60c57.079 0 111.297-18.568 155.785-52.823v-86.216h-86.216C305.044 385.147 281.181 392 256 392z" data-original="#0f9d58" />
+                            <path fill="#31aa52" d="m139.131 325.477-86.308 86.308a260.085 260.085 0 0 0 22.158 25.235C123.333 485.371 187.62 512 256 512V392c-49.624 0-93.117-26.72-116.869-66.523z" data-original="#31aa52" />
+                            <path fill="#3c79e6" d="M512 256a258.24 258.24 0 0 0-4.192-46.377l-2.251-12.299H256v120h121.452a135.385 135.385 0 0 1-51.884 55.638l86.216 86.216a260.085 260.085 0 0 0 25.235-22.158C485.371 388.667 512 324.38 512 256z" data-original="#3c79e6" />
+                            <path fill="#cf2d48" d="m352.167 159.833 10.606 10.606 84.853-84.852-10.606-10.606C388.668 26.629 324.381 0 256 0l-60 60 60 60c36.326 0 70.479 14.146 96.167 39.833z" data-original="#cf2d48" />
+                            <path fill="#eb4132" d="M256 120V0C187.62 0 123.333 26.629 74.98 74.98a259.849 259.849 0 0 0-22.158 25.235l86.308 86.308C162.883 146.72 206.376 120 256 120z" data-original="#eb4132" />
+                        </svg>
+                    </button>
+                    <button type="button"
+                        class="border-none outline-none">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32px" viewBox="0 0 512 512">
+                            <path fill="#1877f2" d="M512 256c0 127.78-93.62 233.69-216 252.89V330h59.65L367 256h-71v-48.02c0-20.25 9.92-39.98 41.72-39.98H370v-63s-29.3-5-57.31-5c-58.47 0-96.69 35.44-96.69 99.6V256h-65v74h65v178.89C93.62 489.69 0 383.78 0 256 0 114.62 114.62 0 256 0s256 114.62 256 256z" data-original="#1877f2" />
+                            <path fill="#fff" d="M355.65 330 367 256h-71v-48.021c0-20.245 9.918-39.979 41.719-39.979H370v-63s-29.296-5-57.305-5C254.219 100 216 135.44 216 199.6V256h-65v74h65v178.889c13.034 2.045 26.392 3.111 40 3.111s26.966-1.066 40-3.111V330z" data-original="#ffffff" />
+                        </svg>
+                    </button>
+                    <button type="button"
+                        class="border-none outline-none">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32px" viewBox="0 0 22.773 22.773">
+                            <path d="M15.769 0h.162c.13 1.606-.483 2.806-1.228 3.675-.731.863-1.732 1.7-3.351 1.573-.108-1.583.506-2.694 1.25-3.561C13.292.879 14.557.16 15.769 0zm4.901 16.716v.045c-.455 1.378-1.104 2.559-1.896 3.655-.723.995-1.609 2.334-3.191 2.334-1.367 0-2.275-.879-3.676-.903-1.482-.024-2.297.735-3.652.926h-.462c-.995-.144-1.798-.932-2.383-1.642-1.725-2.098-3.058-4.808-3.306-8.276v-1.019c.105-2.482 1.311-4.5 2.914-5.478.846-.52 2.009-.963 3.304-.765.555.086 1.122.276 1.619.464.471.181 1.06.502 1.618.485.378-.011.754-.208 1.135-.347 1.116-.403 2.21-.865 3.652-.648 1.733.262 2.963 1.032 3.723 2.22-1.466.933-2.625 2.339-2.427 4.74.176 2.181 1.444 3.457 3.028 4.209z" data-original="#000000"></path>
+                        </svg>
+                    </button>
+                </div> --}}
             </form>
         </div>
     </div>
