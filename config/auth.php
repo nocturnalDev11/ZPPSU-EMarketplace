32414029b51b80439f19bdf189eb1e39bc69889b
Lutreze Hue Jacinto <86185117+nocturnalDev11@users.noreply.github.com>
2024-09-13 02:49:45 +0000

    commit

--- config/auth.php
+++ config/auth.php
@@ -71,7 +71,7 @@ return [
 
         'admins' => [
             'driver' => 'eloquent',
-            'model' => env('ADMIN_AUTH_MODEL', App\Models\Admin::class),
+            'model' => App\Models\Admin::class,
         ],
 
         // 'users' => [
@@ -103,6 +103,12 @@ return [
         'users' => [
             'provider' => 'users',
             'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
+            'expire' => 60,
+            'throttle' => 60,
+        ],
+        'admins' => [
+            'provider' => 'admin',
+            'table' =>  env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
             'expire' => 60,
             'throttle' => 60,
         ],
