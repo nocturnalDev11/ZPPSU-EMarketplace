db7af5ace170296027122978fa0eef6083d0565b
Lutreze Hue Jacinto <86185117+nocturnalDev11@users.noreply.github.com>
2024-09-18 11:12:34 -0400

    Update product-list.blade.php

--- resources/views/livewire/lists/products/product-list.blade.php
+++ resources/views/livewire/lists/products/product-list.blade.php
@@ -17,15 +17,156 @@
         </div>
 
         <!-- Actions -->
-        <div class="flex flex-col items-stretch justify-end w-full space-y-2 md:w-auto md:flex-row md:space-y-0 md:items-center md:space-x-3">
+       <div class="flex items-center justify-end w-full md:w-auto md:flex-row md:space-y-0 xl:gap-0 gap-2 md:items-center md:space-x-3">
             <!-- Add Product -->
-            <button type="button"
-                class="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-purple-700 hover:bg-primary-800">
-                <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
-                </svg>
-                Add product
-            </button>
+            <div x-data="{ modelOpen: false }">
+                <button type="button" @click="modelOpen =!modelOpen"
+                    class="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-purple-700 hover:bg-primary-800">
+                    <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
+                    </svg>
+                    Add product
+                </button>
+
+                <div x-show="modelOpen" class="fixed inset-0 z-50 overflow-y-auto" aria-labelledby="modal-title" role="dialog"
+                    aria-modal="true">
+                    <div class="flex items-end justify-center min-h-screen px-4 text-center md:items-center sm:block sm:p-0">
+                        <div x-cloak @click="modelOpen = false" x-show="modelOpen"
+                            x-transition:enter="transition ease-out duration-300 transform" x-transition:enter-start="opacity-0"
+                            x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in duration-200 transform"
+                            x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
+                            class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-40" aria-hidden="true"></div>
+
+                        <div x-cloak x-show="modelOpen" x-transition:enter="transition ease-out duration-300 transform"
+                            x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+                            x-transition:leave="transition ease-in duration-200 transform"
+                            x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
+                            x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            class="inline-block w-full max-w-xl p-8 my-auto sm:my-24 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl 2xl:max-w-2xl">
+                            <div class="flex items-center justify-between space-x-4">
+                                <h1 class="text-xl font-medium text-gray-800 ">Create a product</h1>
+
+                                <button @click="modelOpen = false" class="text-gray-600 focus:outline-none hover:text-gray-700">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="size-6" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                    </svg>
+                                </button>
+                            </div>
+
+                            <form action="{{ route('product.store') }}" method="POST" class="mt-5" enctype="multipart/form-data">
+                                @csrf
+                                <!-- Photo File Input -->
+                                <div x-data="{ photoName: null, photoPreview: null }" class="col-span-6 ml-2 sm:col-span-4 md:mr-3">
+                                    <input type="file" class="hidden" x-ref="photo" name="prod_picture" x-on:change="
+                                            photoName = $refs.photo.files[0].name;
+                                            const reader = new FileReader();
+                                            reader.onload = (e) => {
+                                                photoPreview = e.target.result;
+                                            };
+                                            reader.readAsDataURL($refs.photo.files[0]);
+                                        ">
+
+                                    <label class="block text-gray-700 text-sm font-bold mb-2 text-center" for="photo">
+                                        Product picture
+                                        <span class="text-red-600"></span>
+                                    </label>
+
+                                    <div class="text-center">
+                                        <!-- Current Profile Photo -->
+                                        <div x-show="!photoPreview"
+                                            class="relative flex items-center justify-center overflow-hidden bg-cover bg-center w-80 h-80 m-auto rounded-md shadow-soft-xl">
+                                            <svg class="w-12 h-12 text-gray-800 dark:text-white" aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                                                viewBox="0 0 24 24">
+                                                <path fill-rule="evenodd" d="M13 10a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2H14a1 1 0 0 1-1-1Z"
+                                                    clip-rule="evenodd" />
+                                                <path fill-rule="evenodd"
+                                                    d="M2 6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12c0 .556-.227 1.06-.593 1.422A.999.999 0 0 1 20.5 20H4a2.002 2.002 0 0 1-2-2V6Zm6.892 12 3.833-5.356-3.99-4.322a1 1 0 0 0-1.549.097L4 12.879V6h16v9.95l-3.257-3.619a1 1 0 0 0-1.557.088L11.2 18H8.892Z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                            <span
+                                                class="absolute inset-y-0 w-full h-full bg-gradient-to-tl from-stone-100/50 to-slate-200/50 opacity-60"></span>
+                                        </div>
+                                        <!-- New Profile Photo Preview -->
+                                        <div class="mt-2" x-show="photoPreview" style="display: none;">
+                                            <span class="block w-80 h-80 rounded-md m-auto shadow bg-center bg-cover"
+                                                x-bind:style="'background-image: url(\'' + photoPreview + '\');'">
+                                            </span>
+                                        </div>
+                                        <button type="button"
+                                            class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-400 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150 mt-2 ml-3"
+                                            x-on:click.prevent="$refs.photo.click()">
+                                            Upload photo
+                                        </button>
+                                    </div>
+                                </div>
+
+                                <div class="grid gap-2 mb-4 grid-cols-2">
+                                    <div class="col-span-2">
+                                        <label for="prod_name" class="block text-sm text-gray-700 dark:text-gray-200">Product
+                                            name</label>
+                                        <input id="prod_name" name="prod_name" placeholder="ex. bag" type="text"
+                                            class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                                    </div>
+
+                                    <div class="col-span-2">
+                                        <label for="prod_price" class="block text-sm text-gray-700 dark:text-gray-200">Price</label>
+                                        <input id="prod_price" name="prod_price" type="number"
+                                            class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                                    </div>
+
+                                    <div class="col-span-2 sm:col-span-1">
+                                        <label for="prod_category"
+                                            class="block text-sm text-gray-700 dark:text-gray-200">Category</label>
+                                        <select id="prod_category" name="prod_category"
+                                            class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                                            <option value="" selected>Select category</option>
+                                            <option value="Apparel">Apparel</option>
+                                            <option value="School supplies">School Supplies</option>
+                                            <option value="Footwares">Footwares</option>
+                                            <option value="Stationery">Stationery</option>
+                                            <option value="Electronics">Electronics</option>
+                                            <option value="University merchandise">University Merchandise</option>
+                                            <option value="Musical instruments">Musical instruments</option>
+                                            <option value="Books">Books</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="col-span-2 sm:col-span-1">
+                                        <label for="prod_condition"
+                                            class="block text-sm text-gray-700 dark:text-gray-200">Condition</label>
+                                        <select id="prod_condition" name="prod_condition"
+                                            class="block w-full px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40">
+                                            <option value="" selected>Select condition</option>
+                                            <option value="New">New</option>
+                                            <option value="Used - like new">Used - Like new</option>
+                                            <option value="Used - like good">Used - Like good</option>
+                                            <option value="Used - like fair">Used - Like fair</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="col-span-2">
+                                        <label for="prod_description"
+                                            class="block text-sm text-gray-700 dark:text-gray-200">Description</label>
+                                        <textarea id="prod_description" name="prod_description" type="text"
+                                            class="scroll-on w-full resize-none block px-3 py-2 mt-2 text-gray-600 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-40"
+                                            rows="4"></textarea>
+                                    </div>
+                                </div>
+
+                                <div class="flex justify-end mt-6">
+                                    <button type="submit"
+                                        class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-md dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:ring-none">
+                                        Add product
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <!-- Sort by -->
             <div @click.away="openSort = false" class="relative">
