a9caa2e23cffff0bc27d8222a91258111a9e6b64
Lutreze Hue Jacinto <86185117+nocturnalDev11@users.noreply.github.com>
2024-09-18 10:40:34 -0400

    Update service-list.blade.php

--- resources/views/livewire/lists/services/service-list.blade.php
+++ resources/views/livewire/lists/services/service-list.blade.php
@@ -20,7 +20,7 @@
         <div class="flex flex-col items-stretch justify-end w-full space-y-2 md:w-auto md:flex-row md:space-y-0 md:items-center md:space-x-3">
             <!-- Add service -->
             <div x-data="{ modelOpen: false }">
-                <button type="button" @click="modelOpen =!modelOpen" type="button"
+                <button type="button" @click="modelOpen =!modelOpen"
                     class="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-purple-700 hover:bg-primary-800">
                     <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
